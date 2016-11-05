df49062417c9ea1f50e1cd09a58b017019aca2c5
Najma Dawood-McCarthy <Najmad@users.noreply.github.com>
2016-11-04 20:49:17 -0400

    Add files via upload

---
+++ Najma CC_Ex5/sketch.js
@@ -0,0 +1,14 @@
+var backgroundpic;
+
+function preload() {
+backgroundpic =  loadImage("images/background.jpg")
+}
+
+function setup() {
+  createCanvas(400, 600);
+  background(backgroundpic, 0, 0);
+}
+
+function draw() {
+  
+}
